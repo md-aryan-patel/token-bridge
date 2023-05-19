6f7b7cb045c74973a931d1ddff96ab1cd6b6ad19
Aryan Patel <122960735+md-aryan-patel@users.noreply.github.com>
2023-05-19 16:54:05 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,25 +1,25 @@
-const sep_XBTC = "0x984c354Bb6Fd7Ea8DC3A234A551B4a4fdba68899";
-const sep_XETH = "0x1b59C73ac3673Af719bd51CEd2418d514B930d1A";
-const sep_swap = "0x79b3AaCbDCb8e513Bb100707efBeA1062bc5b248";
+const sep_XBTC = "";
+const sep_XETH = "";
+const sep_swap = "";
 
-const pol_XBTC = "0xC8134bdd0aDfe1bd2a7950f0e867F5C34201b317";
-const pol_XETH = "0xa3B37e6c8941a26b7606A9030F54f39B615c2b21";
-const pol_swap = "0x4E3Ca48820bd4e039566E2Aa5Fe613687F4e981c";
+const pol_XBTC = "";
+const pol_XETH = "";
+const pol_swap = "";
 
 const polygon_network =
-  "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78";
+  "https://polygon-mumbai.infura.io/v3/{apikey}";
 const sepolia_network =
-  "https://sepolia.infura.io/v3/4ec1f52fc49f48fb9f8bac27b1348a42";
+  "https://sepolia.infura.io/v3/{apikey}";
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 const user_address = "0x5f284edE3b97829fAd8b4Ed1d973c51962C1972a";
 const user_private_key =
-  "9eb953fdd379696c2fba178125fc77297da5481054376add40f210402f8f0f8a";
+  "";
 
 const admin_address = "0x80A344d8095d099bb72e6298aA8bA2C9E82A4Cbe";
 const admin_private_key =
-  "ecfc25e59cd52529212388d1591131974c2cefbd9e0993e9ccdb5fa2b112da95";
+  "";
 
 export default {
   ZERO_ADDRESS,
